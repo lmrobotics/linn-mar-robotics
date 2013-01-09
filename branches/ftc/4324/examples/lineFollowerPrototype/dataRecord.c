98d96a5f4eadcb3671a51012fa8bf968834ccc6e
raflood61@gmail.com <raflood61@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2013-01-09 11:54:20 +0000

    Move examples to branches

---
+++ branches/ftc/4324/examples/lineFollowerPrototype/dataRecord.c
@@ -0,0 +1,34 @@
+const int   maxDataLog = 1024*8;
+static byte tempData[maxDataLog];
+int         logSize = 0;
+
+/** ***************************************************************************
+ * \brief
+ */
+void saveLog()
+{
+  TFileHandle   fileHandle;
+  TFileIOResult ioResult;
+  int temp = logSize;
+  string sFileName = "dataRecord.dat";
+  Delete(sFileName, ioResult);
+  OpenWrite(fileHandle, ioResult, sFileName, temp);
+  for(int i=0; i<=temp; i++)
+  {
+    WriteByte(fileHandle, ioResult, tempData[i]);
+  }
+
+  Close(fileHandle, ioResult);
+
+} // saveLog
+
+/** ***************************************************************************
+ * \brief
+ */
+void logByte(byte logDat)
+{
+  if(logSize < maxDataLog)
+  {
+    tempData[logSize++] = logDat;
+  }
+} // logByte
