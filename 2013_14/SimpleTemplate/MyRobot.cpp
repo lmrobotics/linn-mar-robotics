65a2faea2ceb9038e1dc77bc972f0f9e1704abf0
raflood61@gmail.com <raflood61@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2013-10-05 14:22:42 +0000

    Fixed drive code, added the start of datalogger code

--- 2013_14/SimpleTemplate/MyRobot.cpp
+++ 2013_14/SimpleTemplate/MyRobot.cpp
@@ -1,20 +1,19 @@
 #include "WPILib.h"
 #include "GenericHID.h"
 #include "Dashboard.h"
+#include "dualMotor.h"
+#include "dataLogger.h"
 
 class Motors
 {
 	Victor drive1,drive2,drive3,drive4;
-	Talon shooter1, shooter2;
 	
 public:
 	Motors(void):
 		drive1(1,1),
 		drive2(1,2),
 		drive3(1,3),
-		drive4(1,4),
-		shooter1(1,5),
-		shooter2(1,6)
+		drive4(1,4)
 		{
 		}
 	
@@ -49,13 +48,7 @@ public:
 		drive3.Set(0);
 		drive4.Set(0);
 	}
-	
-	void spinshooter(float speed){
-		shooter1.Set(speed);
-		shooter2.Set(speed);
-	}
 };
-
 
 class RobotDemo : public SimpleRobot
 {
@@ -64,7 +57,11 @@ class RobotDemo : public SimpleRobot
 	Motors motors;
 	Encoder shooter_encoder;
 	Compressor compressor;
-	Solenoid shooter2, shooter1;
+	Solenoid shooter2, shooter1, lifter1, lifter2;
+	dualMotor <Talon> shooter_motors;
+	PIDController shootControl;
+	dataLogger motorRecord;
+	dataLogger encoderRecord;
 
 public:
 	RobotDemo(void):
@@ -74,10 +71,18 @@ public:
 		shooter_encoder(1,2),
 		compressor(3,1),
 		shooter2(1),
-		shooter1(2)
+		shooter1(2),
+		lifter1(3),
+		lifter2(4),
+		shooter_motors(1,5,6),
+		shootControl(0.2, 0.02, 0.0, &shooter_encoder, &shooter_motors),
+		motorRecord("MotorData.txt"),
+		encoderRecord("EncoderData.txt")
 	{
 		shooter_encoder.SetMaxPeriod(.1);
 		shooter_encoder.SetDistancePerPulse(.06);
+		shooter_encoder.SetPIDSourceParameter(Encoder::kRate);
+		shootControl.SetOutputRange(-0.50, 0.50);
 		shooter_encoder.Start();
 	}
 
@@ -94,12 +99,24 @@ public:
 		float LeftSpeed,RightSpeed;
 		while (IsOperatorControl())
 		{
+			//*********VARIABLE DEFINITIONS************
+			//Axes**
 			float y = xbox.GetY(GenericHID::kLeftHand);
 			float x = xbox.GetRawAxis(4);
-			bool second_a_button = xbox2.GetRawButton(1);
+			//******
+			
+			//Buttons on controller 1
 			bool a_button = xbox.GetRawButton(1);
+			
+			//Buttons on controller 2
+			bool second_a_button = xbox2.GetRawButton(1);
+			bool second_left_button = xbox2.GetRawButton(5);
+			
+			//Variables from sensors
 			double encoder_count = shooter_encoder.GetRate();
 			
+			
+			//Starts compressor, charges to switch value
 			if(!compressor.GetPressureSwitchValue()){
 				compressor.Start();
 			}
@@ -107,14 +124,26 @@ public:
 				compressor.Stop();
 			}
 			
-			
+			//Wheel Speed
 			if(second_a_button==true) {
-				motors.spinshooter(.35);
+				shootControl.SetSetpoint(20);
+				shootControl.Enable();
 			}
 			else {
-				motors.spinshooter(0);
+				shootControl.Disable();
 			}
 			
+			//Lifter Cylinders
+			if(second_left_button == true) {
+				lifter1.Set(true);
+				lifter2.Set(false);
+			}
+			else {
+				lifter1.Set(false);
+				lifter2.Set(true);
+			}
+			
+			//Shooter Cylinder
 			if(a_button==true){
 				shooter1.Set(true);
 				shooter2.Set(false);
@@ -124,14 +153,31 @@ public:
 				shooter2.Set(true);
 			}
 			
+			//******ARCADE DRIVE CODE*****
+			//square the controller inputs
+			if (y<0){
+				y = -(y*y);
+			}
+			else{
+				y = y*y;
+			}
+			if (x<0){
+				x = -(x*x);
+			}
+			else {
+				x = x*x;
+			}
+			
+			//calculate the power for each side of drive
+			//up on y-axis is negative
 			if(y >= deadband) {
 				if(x > deadband) {
 					LeftSpeed=max(x,y)*maxout;
-					RightSpeed=(y+x)*maxout;
+					RightSpeed=(y-x)*maxout;
 
 				}
 				else if(x < -deadband){
-					LeftSpeed=(-x+y)*maxout;
+					LeftSpeed=(y+x)*maxout;
 					RightSpeed=max(-x,y)*maxout;
 				}
 				else {
@@ -143,7 +189,6 @@ public:
 				if(x > deadband) {
 					LeftSpeed=(x+y)*maxout;
 					RightSpeed=-max(x,-y)*maxout;
-
 				}
 				else if(x < -deadband) {
 					LeftSpeed=-max(-x,-y)*maxout;
@@ -168,6 +213,7 @@ public:
 					RightSpeed = 0;
 				}
 			}
+			//make sure power is not over one
 			if(y>1){
 				y=1;
 			}
@@ -180,10 +226,19 @@ public:
 			else if(x<-1){
 				x=-1;
 			}
+			
+			
+			//Print Statements to Console
 			printf("EncoderCount %f \n",encoder_count);
 			
 			
+			
 			motors.drive(-LeftSpeed,RightSpeed);
+			
+			if (shootControl.IsEnabled()){
+				motorRecord.write_(shooter_motors.Get());
+				encoderRecord.write_(shooter_encoder.GetRate());
+			}
 			
 			Wait(0.005);
 		}
