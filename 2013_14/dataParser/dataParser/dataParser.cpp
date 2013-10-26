ba8e498bb64c771bd48fc8e884c972b1eaaeeb2c
lmroboticssw@gmail.com <lmroboticssw@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2013-10-26 15:13:40 +0000

    added columns capability

--- 2013_14/dataParser/dataParser/dataParser.cpp
+++ 2013_14/dataParser/dataParser/dataParser.cpp
@@ -6,15 +6,29 @@ using namespace std;
 // our main
 int main(int argc, char* argv[])
 {
+	int numColumns = 1;
+	if (argc > 2)
+	{
+		try
+		{
+			numColumns = stoi(argv[2]);
+		}
+		catch (exception e)
+		{
+			cout << "Error: " << e.what();
+		}
+	}
+
    if (argc > 1)
    {
    dataParserClass *dp = new dataParserClass(argv[1]);
-   dp->parseFloatsReversed();
+   dp->parseFloatsReversed(numColumns);
    return 0;
    }
    else {
+
       dataParserClass *dp = new dataParserClass("U:\\dataParser\\byteSwappedFloats.bin");
-      dp->parseFloatsReversed();
+      dp->parseFloatsReversed(numColumns);
       return 0;
    }
 }
