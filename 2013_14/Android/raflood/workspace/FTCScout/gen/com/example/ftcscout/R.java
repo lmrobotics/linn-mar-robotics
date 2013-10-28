39248a1e59672429268bc08d19be2c4ca687fafe
raflood61 <raflood61@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2013-10-28 01:21:13 +0000

    Kinda works, needs a lot of clean up

--- 2013_14/Android/raflood/workspace/FTCScout/gen/com/example/ftcscout/R.java
+++ 2013_14/Android/raflood/workspace/FTCScout/gen/com/example/ftcscout/R.java
@@ -48,8 +48,10 @@ public final class R {
         public static final int action_search=0x7f050007;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int bt_share_picker_label=0x7f050009;
         public static final int button_send=0x7f050004;
         public static final int hello_world=0x7f050006;
+        public static final int process=0x7f050008;
         public static final int team_name_prompt=0x7f050002;
         public static final int team_number_prompt=0x7f050003;
         public static final int title_activity_display_message=0x7f050005;
