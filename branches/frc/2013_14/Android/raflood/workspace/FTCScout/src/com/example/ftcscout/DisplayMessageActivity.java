b0271f391e52ae89a35b8a0d35d0bf4414e246da
raflood61 <raflood61@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2013-10-28 01:21:13 +0000

    Kinda works, needs a lot of clean up

--- branches/frc/2013_14/Android/raflood/workspace/FTCScout/src/com/example/ftcscout/DisplayMessageActivity.java
+++ branches/frc/2013_14/Android/raflood/workspace/FTCScout/src/com/example/ftcscout/DisplayMessageActivity.java
@@ -1,10 +1,12 @@
 package com.example.ftcscout;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.widget.TextView;
 import android.content.pm.PackageManager;
@@ -12,11 +14,17 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.Context;
 import android.util.Log;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.OutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import android.bluetooth.BluetoothAdapter;
 
 public class DisplayMessageActivity extends Activity {
 
@@ -34,18 +42,75 @@ public class DisplayMessageActivity extends Activity {
 	    Intent intent = getIntent();
 	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 	    int x = intent.getIntExtra ("NumberPickerValue", -1);
+	    message = message + " " + Integer.toString(x) + " points";
 
 	    // Create the text view
 	    TextView textView = new TextView(this);
 	    textView.setTextSize(40);
-	    textView.setText(message + " " + Integer.toString(x) + " points");
+	    textView.setText(message);
 
 	    // Set the text view as the activity layout
 	    setContentView(textView);
-	    
+	    String fileName = "ftc_scores____.csv";
 	    // Write our file
-	    writeFile(ctx);
-	}
+	    writeFile(ctx, fileName);
+	    
+		// inside method
+	    // Check if bluetooth is supported
+	    BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+	    if (btAdapter == null) {
+           // Device does not support Bluetooth
+           message = message + "\n Blue tooth not found on this device";
+   	       textView.setText(message);
+        }
+        else
+        {
+            try {
+           	 InputStream inputStream = openFileInput(fileName);
+           	 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+           	 BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+           	 message = message + "\n" + bufferedReader.readLine();
+     	     textView.setText(message);
+           }
+           catch  (Exception e) {  
+           }        
+
+            // send file via blue tooth
+            message = message + "\n Sending data via Blue tooth";
+    	    textView.setText(message);
+    	    File file = new File(ctx.getFilesDir().getPath(), fileName);
+    	    if (file.exists())
+    	    {
+                message = message + "\n Transmitting!";
+        	    textView.setText(message);
+        	    Intent bti = new Intent();
+        	    bti.setAction(Intent.ACTION_SEND);
+        	    //bti.setComponent(new ComponentName(
+        	    //        "com.android.bluetooth",
+        	    //        "com.android.bluetooth.opp.BluetoothOppLauncherActivity"));
+        	    bti.setType("*/*");
+        	    //bti.setComponent(new ComponentName("com.android.bluetooth", "com.android.bluetooth.opp.BluetoothOppLauncherActivity"));
+      	        bti.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file) );
+      	        try
+      	        {
+        	       startActivity(bti);
+      	        }
+    		    catch (Exception e) {
+                    message = message + "\n" + e;
+            	    textView.setText(message);
+   		    	     Log.e("MYAPP", "file write failed", e);
+
+   		        } 
+    	    }
+    	    else
+    	    {
+                message = message + "\n File does not exist";
+        	    textView.setText(message);
+    	    	
+    	    }
+
+        }
+    }
 
 	/**
 	 * Set up the {@link android.app.ActionBar}.
@@ -57,27 +122,29 @@ public class DisplayMessageActivity extends Activity {
 	/*
 	 * Create and write data to a file
 	 */
-	private void writeFile(Context ctx) {
+	private void writeFile(Context ctx, String fileName) {
 		PackageManager m = getPackageManager();
 		String s = getPackageName();
 		try {
 		    PackageInfo p = m.getPackageInfo(s, 0);
 		    s = p.applicationInfo.dataDir;
 		} catch (NameNotFoundException e) {
-		    Log.w("yourtag", "Error Package name not found ", e);
+		    Log.w("MYAPP", "Error Package name not found ", e);
 		}
 		
-		// Create File and write data
-		String filePath = s + File.separator + "test.txt";
-		String data = "Hello!";
-		 try {
-			    FileOutputStream os = ctx.openFileOutput(filePath, Context.MODE_PRIVATE);
+        // Create File and write data
+		String data = "Hello There!!!!";
+        try {
+        	    //File file = new File(ctx.getFilesDir(), fileName);
+        	    //file.mkdirs();
+        	    //file.createNewFile();
+                FileOutputStream os = ctx.openFileOutput(fileName, Context.MODE_PRIVATE);
 		        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(os);
 		        outputStreamWriter.write(data);
 		        outputStreamWriter.close();
 		    }
-		    catch (IOException e) {
-		    	 Log.e("MYAPP", "exception", e);
+		    catch (Exception e) {
+		    	 Log.e("MYAPP", "file write failed", e);
 
 		    } 
 	}
--- branches/frc/2013_14/Android/raflood/workspace/FTCScout/src/com/example/ftcscout/MainActivity.java
+++ branches/frc/2013_14/Android/raflood/workspace/FTCScout/src/com/example/ftcscout/MainActivity.java
@@ -1,15 +1,32 @@
 package com.example.ftcscout;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.widget.EditText; 
 import android.widget.NumberPicker;
 import android.view.MenuInflater;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 
 
 public class MainActivity extends Activity {
@@ -54,17 +71,105 @@ public class MainActivity extends Activity {
 	        default:
 	            return super.onOptionsItemSelected(item);
 	    }
-	}
+	} 
 	
 	/** Called when the user clicks the Send button */
 	public void sendMessage(View view) {
 		Intent intent = new Intent(this, DisplayMessageActivity.class);
+	    String fileName = "ftc_score.csv";
+		Context ctx = getApplicationContext();
+	    writeFile(ctx, fileName);
+	    //sendFileViaBluetooth("hello_file.txt");
+
+	    sendFileViaBluetooth(fileName);
+	    
 		EditText editText = (EditText) findViewById(R.id.edit_message);
 		String message = editText.getText().toString();
 		NumberPicker np = (NumberPicker) findViewById(R.id.NumberPicker01);
 		int x = np.getValue();
 		intent.putExtra(EXTRA_MESSAGE, message);
 		intent.putExtra("NumberPickerValue", x);
-		startActivity(intent);
+		//startActivity(intent);
 	}
+
+	private void sendFileViaBluetooth(String fileName)
+	{
+		Context ctx = getApplicationContext();
+		File file = new File(Environment.getExternalStorageDirectory().toString() + "/Pictures/Family", fileName);
+		File file2 = new File(ctx.getFilesDir().getPath(), fileName);
+		
+		if (file.exists())
+		{
+	   	    Intent bti = new Intent();
+	   	    bti.setAction(Intent.ACTION_SEND);
+	   	    //bti.setComponent(new ComponentName(
+	   	    //        "com.android.bluetooth",
+    	    //        "com.android.bluetooth.opp.BluetoothOppLauncherActivity"));
+	    	bti.setType("text/plain");
+	        //bti.setComponent(new ComponentName("com.android.bluetooth", "com.android.bluetooth.opp.BluetoothOppLauncherActivity"));
+	        bti.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file) );
+  	        try
+  	        {
+    	       startActivity(bti);
+  	        }
+		    catch (Exception e) {
+		     Log.e("MYAPP", "file write failed", e);
+		    } 
+		}
+		    		
+	} // sendFileViaBluetooth
+	
+	/*
+	 * Create and write data to a file
+	 */
+	private void writeFile(Context ctx, String fileName) {
+		PackageManager m = getPackageManager();
+		String s = getPackageName();
+		try {
+		    PackageInfo p = m.getPackageInfo(s, 0);
+		    s = p.applicationInfo.dataDir;
+		} catch (NameNotFoundException e) {
+		    Log.w("MYAPP", "Error Package name not found ", e);
+		}
+		
+        // Create File and write data
+		String data = "Hello World!\n";
+        try 
+        {
+    	    //File file = new File(ctx.getFilesDir().getPath(), fileName);
+    	    //file.mkdirs();
+    	    //file.createNewFile();
+
+            String path = Environment.getExternalStorageDirectory().toString() + "/Pictures/Family";
+    		File file1 = new File(path, fileName);
+    		file1.createNewFile();
+        	FileOutputStream os = new FileOutputStream(file1);
+        	OutputStreamWriter outputStreamWriter = new OutputStreamWriter(os);
+            outputStreamWriter.write(data);
+            outputStreamWriter.write(data);
+            outputStreamWriter.write(data);
+            outputStreamWriter.write(data);
+            outputStreamWriter.write(data);
+            outputStreamWriter.write(data);
+            outputStreamWriter.write(data);	
+            outputStreamWriter.write(data);
+            outputStreamWriter.write(data);
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+            String FILENAME = "hello_file.txt";
+            String string = "hello world!";
+    		File file2 = new File(path, FILENAME);
+            FileOutputStream fos = new FileOutputStream(file2);
+            fos.write(string.getBytes());
+            fos.close();
+	    }
+	    catch (Exception e) {
+	    	 Log.e("MYAPP", "file write failed", e);
+
+	    }         
+                
+        
+	} // writeFile
+
+
 }
