bf8a2a41860664819d627904db6bce26a4c28fe8
raflood61 <raflood61@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2013-10-29 01:12:16 +0000

    Cleanup

--- 2013_14/Android/raflood/workspace/FTCScout/gen/com/example/ftcscout/R.java
+++ 2013_14/Android/raflood/workspace/FTCScout/gen/com/example/ftcscout/R.java
@@ -31,8 +31,8 @@ public final class R {
         public static final int action_settings=0x7f080007;
         public static final int button=0x7f080002;
         public static final int checkBox1=0x7f080006;
-        public static final int editText1=0x7f080001;
-        public static final int edit_message=0x7f080000;
+        public static final int teamName=0x7f080000;
+        public static final int teamNumber=0x7f080001;
         public static final int textView1=0x7f080004;
     }
     public static final class layout {
