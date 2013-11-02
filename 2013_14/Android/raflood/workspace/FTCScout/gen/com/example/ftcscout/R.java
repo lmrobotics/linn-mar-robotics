340c0edfcf8e9b1da68a58b4a33007dbe6c745da
raflood61 <raflood61@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2013-11-02 12:43:20 +0000

    Added menu buttons to save record and send file

--- 2013_14/Android/raflood/workspace/FTCScout/gen/com/example/ftcscout/R.java
+++ 2013_14/Android/raflood/workspace/FTCScout/gen/com/example/ftcscout/R.java
@@ -21,13 +21,16 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_save=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_action_send=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int NumberPicker01=0x7f080003;
         public static final int NumberPicker02=0x7f080005;
-        public static final int action_search=0x7f080008;
+        public static final int action_save=0x7f080009;
+        public static final int action_send=0x7f080008;
         public static final int action_settings=0x7f080007;
         public static final int button=0x7f080002;
         public static final int checkBox1=0x7f080006;
@@ -45,16 +48,18 @@ public final class R {
         public static final int main_activity_actions=0x7f070002;
     }
     public static final class string {
-        public static final int action_search=0x7f050007;
+        public static final int action_save=0x7f050002;
+        public static final int action_search=0x7f050009;
+        public static final int action_send=0x7f050003;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int bt_share_picker_label=0x7f050009;
-        public static final int button_send=0x7f050004;
-        public static final int hello_world=0x7f050006;
-        public static final int process=0x7f050008;
-        public static final int team_name_prompt=0x7f050002;
-        public static final int team_number_prompt=0x7f050003;
-        public static final int title_activity_display_message=0x7f050005;
+        public static final int bt_share_picker_label=0x7f05000b;
+        public static final int button_send=0x7f050006;
+        public static final int hello_world=0x7f050008;
+        public static final int process=0x7f05000a;
+        public static final int team_name_prompt=0x7f050004;
+        public static final int team_number_prompt=0x7f050005;
+        public static final int title_activity_display_message=0x7f050007;
     }
     public static final class style {
         /** 
