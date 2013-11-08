8b6cac4d131e48dca25149cbb50edfef23ef0019
zacadkins.public@gmail.com <zacadkins.public@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2013-11-08 04:39:25 +0000

    Adding snapshot of SW as of 11/7/13

---
+++ trunk/ftc/4324/2013/Tag/Bryan/SensorControl.c
@@ -0,0 +1,23 @@
+
+task main()
+{
+
+	if (sensorValue[sonar]<20)
+	{
+		motor[motorL]=100;
+		motor[motorR]=100;
+	}
+	else
+	{
+		motor[motor]=0;
+		motor[motor]=0;
+	}
+
+	while( sonar value is less than 10inch)
+{
+	move forward;
+}
+stop;
+
+
+}
