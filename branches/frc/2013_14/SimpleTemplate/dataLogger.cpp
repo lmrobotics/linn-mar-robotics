7aeb59cc42a076e16cafb9291368da94a99c7cc1
raflood61@gmail.com <raflood61@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2013-11-23 17:23:28 +0000

    Data logger now works as intended

--- branches/frc/2013_14/SimpleTemplate/dataLogger.cpp
+++ branches/frc/2013_14/SimpleTemplate/dataLogger.cpp
@@ -3,30 +3,23 @@
 #include "dataLogger.h"
 #include "ioLib.h"
 
-
-dataLogger::dataLogger (const char* fileName): buffer(0)
+dataLogger::dataLogger (const char* fileName)
 {
 	fd = fopen(fileName, "w");
-	buffer = new char[buffer_size]();
-	buffer_index = 0;
 }
 
 dataLogger::~dataLogger ()
 {
-}
-
-
-void dataLogger::write_float(float value)
-{
-    if((buffer_index + sizeof(value)) <= buffer_size) 
-    {
-	   memcpy(&buffer[buffer_index], (void*)&value, sizeof(value));
-	   buffer_index += sizeof(value);
-    }
+	fclose(fd);
 }
 
 void dataLogger::close_()
 {
-	fwrite(buffer, 1, buffer_index, fd);
 	fclose(fd);
+}
+
+void dataLogger::write_float(float value)
+{
+	char* temp=(char*)(&value);
+	fwrite(temp, 1, sizeof(value), fd);
 }
