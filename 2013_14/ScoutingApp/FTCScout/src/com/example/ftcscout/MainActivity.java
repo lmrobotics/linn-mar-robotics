c80b1d3a90d758e94248b23f3757d107aacd475c
lmroboticssw <lmroboticssw@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2013-12-06 02:50:10 +0000

    Working Grid layout

--- 2013_14/ScoutingApp/FTCScout/src/com/example/ftcscout/MainActivity.java
+++ 2013_14/ScoutingApp/FTCScout/src/com/example/ftcscout/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.gridlayoutftc);
         NumberPicker np = (NumberPicker) findViewById(R.id.Driver1pt);
         np.setMaxValue(100);
         np.setMinValue(0);
