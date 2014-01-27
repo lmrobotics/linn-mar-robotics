780c73c0916c036d10d17ba6c07f825426bcc00a
t.nandan2008@gmail.com <t.nandan2008@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-01-27 00:10:24 +0000

    Rating system added. Background is functional. Tested on AVD. Competition and Team number overlap fixed.

--- branches/frc/2013_14/FRCGameScoutingApp/FRCGame/gen/com/example/frcgame/R.java
+++ branches/frc/2013_14/FRCGameScoutingApp/FRCGame/gen/com/example/frcgame/R.java
@@ -8,6 +8,9 @@
 package com.example.frcgame;
 
 public final class R {
+    public static final class array {
+        public static final int spinchoice=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -21,54 +24,57 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bc=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int Auto_Bonus=0x7f080003;
-        public static final int Autonomous=0x7f080017;
-        public static final int Blocked_Shots=0x7f080007;
-        public static final int Blocked_Shots_T=0x7f080023;
-        public static final int Catches=0x7f080015;
-        public static final int Catches_Lable=0x7f080014;
-        public static final int Comments=0x7f080009;
-        public static final int Comments_Auto=0x7f080008;
-        public static final int Comments_T=0x7f080010;
-        public static final int Comments_Tele=0x7f08000f;
-        public static final int Foul=0x7f080004;
-        public static final int Foul_T=0x7f080021;
-        public static final int GridLayout1=0x7f080000;
-        public static final int High_Goal=0x7f080011;
-        public static final int High_Goal_Lable=0x7f080013;
-        public static final int Hot_Goals=0x7f080002;
-        public static final int Low_Goal=0x7f080016;
-        public static final int Low_Goal_Lable=0x7f080012;
-        public static final int Mechanical_Problems=0x7f080006;
-        public static final int Mechanical_Problems_T=0x7f080020;
-        public static final int Mobility_Bonus=0x7f080001;
-        public static final int Mobility_Bonus_T=0x7f08000e;
-        public static final int RecA1=0x7f08001c;
-        public static final int RecA2=0x7f08001e;
-        public static final int RecA3=0x7f08001d;
-        public static final int SentA1=0x7f08001a;
-        public static final int SentA2=0x7f08001b;
-        public static final int SentA3=0x7f080019;
-        public static final int TeamNo=0x7f08001f;
-        public static final int Tech_Foul=0x7f080005;
-        public static final int Tech_Foul_T=0x7f080022;
-        public static final int Tele_op=0x7f08000a;
-        public static final int Truss_T=0x7f08000b;
-        public static final int action_settings=0x7f080026;
-        public static final int dAuto=0x7f080024;
-        public static final int editText1=0x7f080025;
-        public static final int senddata=0x7f080018;
-        public static final int three_assist_T=0x7f08000d;
-        public static final int two_assist_T=0x7f08000c;
+        public static final int Auto_Bonus=0x7f090003;
+        public static final int Autonomous=0x7f090017;
+        public static final int Blocked_Shots=0x7f090007;
+        public static final int Blocked_Shots_T=0x7f090023;
+        public static final int Catches=0x7f090015;
+        public static final int Catches_Lable=0x7f090014;
+        public static final int Comments=0x7f090009;
+        public static final int Comments_Auto=0x7f090008;
+        public static final int Comments_T=0x7f090010;
+        public static final int Comments_Tele=0x7f09000f;
+        public static final int Foul=0x7f090004;
+        public static final int Foul_T=0x7f090021;
+        public static final int GridLayout1=0x7f090000;
+        public static final int High_Goal=0x7f090011;
+        public static final int High_Goal_Lable=0x7f090013;
+        public static final int Hot_Goals=0x7f090002;
+        public static final int Low_Goal=0x7f090016;
+        public static final int Low_Goal_Lable=0x7f090012;
+        public static final int Mechanical_Problems=0x7f090006;
+        public static final int Mechanical_Problems_T=0x7f090020;
+        public static final int Mobility_Bonus=0x7f090001;
+        public static final int Mobility_Bonus_T=0x7f09000e;
+        public static final int RecA1=0x7f09001c;
+        public static final int RecA2=0x7f09001e;
+        public static final int RecA3=0x7f09001d;
+        public static final int SentA1=0x7f09001a;
+        public static final int SentA2=0x7f09001b;
+        public static final int SentA3=0x7f090019;
+        public static final int TeamNo=0x7f09001f;
+        public static final int Tech_Foul=0x7f090005;
+        public static final int Tech_Foul_T=0x7f090022;
+        public static final int Tele_op=0x7f09000a;
+        public static final int Truss_T=0x7f09000b;
+        public static final int action_settings=0x7f090028;
+        public static final int dAuto=0x7f090024;
+        public static final int editText1=0x7f090025;
+        public static final int senddata=0x7f090018;
+        public static final int spinner1=0x7f090027;
+        public static final int textView1=0x7f090026;
+        public static final int three_assist_T=0x7f09000d;
+        public static final int two_assist_T=0x7f09000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -97,10 +103,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
