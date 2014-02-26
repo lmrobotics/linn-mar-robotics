c985ff83cf85a669d74ccb94f839f93c417f0141
raflood61 <raflood61@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-02-26 03:52:09 +0000

    Pre-peoria code

---
+++ branches/frc/2013_14/robotCode/IterativeTemplate/floorPickUp.h
@@ -0,0 +1,21 @@
+#ifndef FloorPickUp_H
+#define FloorPickUp_H
+#include "WPILib.h"
+
+class FloorPickUp {
+public:
+	FloorPickUp (uint8_t moduleNumber1, uint32_t channel1, uint8_t moduleNumber2, uint32_t channel2,
+			uint8_t moduleNumber3, uint32_t channel3);
+	~FloorPickUp();
+	void holdPos(float pos, float currentPos);
+	void goToPos(float pos, float currentPos);
+	void stop();
+	void moveAngle(float power, int direction);
+	void moveWheels(float power);
+private:
+	Talon armLeft, armRight;
+	Victor armWheel;
+	AnalogChannel *Pot;
+};
+	
+#endif
