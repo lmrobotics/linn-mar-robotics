6a19bdee80176e9d92e2edb6633dd12c621ffe5a
t.nandan2008@gmail.com <t.nandan2008@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-02-26 01:45:14 +0000

    File save re-added

--- branches/frc/2013_14/FRCGameScoutingApp/FRCGame/src/com/example/frcgame/objectStorage.java
+++ branches/frc/2013_14/FRCGameScoutingApp/FRCGame/src/com/example/frcgame/objectStorage.java
@@ -6,6 +6,7 @@ import java.io.OutputStreamWriter;
 public final class objectStorage {
 
 	static StringBuilder txtpass = new StringBuilder();
+	static StringBuilder scores = new StringBuilder();
 	static String alliance;
 	static FileOutputStream outputStream;
 	static OutputStreamWriter outputStreamWriter;
