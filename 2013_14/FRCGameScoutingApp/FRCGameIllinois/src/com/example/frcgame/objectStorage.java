0b6368f14113861859d90fe51ce084a4d783ba6a
t.nandan2008@gmail.com <t.nandan2008@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-02-28 03:35:31 +0000

    This is the version without the timing data, but a new fouls spinner.

---
+++ 2013_14/FRCGameScoutingApp/FRCGameIllinois/src/com/example/frcgame/objectStorage.java
@@ -0,0 +1,15 @@
+package com.example.frcgame;
+
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+public final class objectStorage {
+
+	static StringBuilder txtpass = new StringBuilder();
+	static StringBuilder scores = new StringBuilder();
+	static String alliance;
+	static FileOutputStream outputStream;
+	static FileOutputStream outputStreams;
+	static OutputStreamWriter outputStreamWriter;
+	static OutputStreamWriter outputStreamWriters;
+}
