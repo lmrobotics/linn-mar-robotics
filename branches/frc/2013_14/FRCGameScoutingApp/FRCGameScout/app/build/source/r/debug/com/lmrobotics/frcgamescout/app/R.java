5f0f214260dccaec7a10aef51254948ce26b8e85
t.nandan2008@gmail.com <t.nandan2008@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-03-08 16:59:31 +0000

    work well-ish

--- branches/frc/2013_14/FRCGameScoutingApp/FRCGameScout/app/build/source/r/debug/com/lmrobotics/frcgamescout/app/R.java
+++ branches/frc/2013_14/FRCGameScoutingApp/FRCGameScout/app/build/source/r/debug/com/lmrobotics/frcgamescout/app/R.java
@@ -19,13 +19,18 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080003;
-        public static final int horizontalScrollView=0x7f080002;
         public static final int pager=0x7f080000;
         public static final int section_label=0x7f080001;
+        public static final int textView=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int auto=0x7f030001;
+        public static final int comments=0x7f030002;
+        public static final int fragment_main=0x7f030003;
+        public static final int save=0x7f030004;
+        public static final int setup=0x7f030005;
+        public static final int teleop=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- branches/frc/2013_14/FRCGameScoutingApp/FRCGameScout/app/src/main/java/com/lmrobotics/frcgamescout/app/MainActivity.java
+++ branches/frc/2013_14/FRCGameScoutingApp/FRCGameScout/app/src/main/java/com/lmrobotics/frcgamescout/app/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
@@ -25,11 +26,8 @@ public class MainActivity extends Activity {
      * fragments for each of the sections. We use a
      * {@link FragmentPagerAdapter} derivative, which will keep every
      * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
      * {@link android.support.v13.app.FragmentStatePagerAdapter}.
      */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
     /**
      * The {@link ViewPager} that will host the section contents.
      */
@@ -39,17 +37,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.setCurrentItem(0);
+        MyPagerAdapter adapter = new MyPagerAdapter();
+        ViewPager myPager = (ViewPager) findViewById(R.id.pager);
+        myPager.setAdapter(adapter);
+        myPager.setCurrentItem(0);
 
     }
 
@@ -72,47 +63,6 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
-            }
-            return null;
-        }
-
     }
 
     /**
