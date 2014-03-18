e2280cd369b9b8ab36debd30650739bc852b4aae
t.nandan2008@gmail.com <t.nandan2008@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-03-18 15:35:38 +0000

    Uploading seemingly functional build

---
+++ branches/frc/2013_14/FRCGameScoutingApp/FRCGameScoutFinal/app/src/main/java/com/lmrobotics/frcgamescoutfinal/app/ObjStor.java
@@ -0,0 +1,13 @@
+package com.lmrobotics.frcgamescoutfinal.app;
+
+/**
+ * Created by TV Family on 3/17/14.
+ */
+
+
+public class ObjStor {
+    public static int MatchID;
+    public static boolean matchReplay;
+    public static int TeamNo;
+
+}
