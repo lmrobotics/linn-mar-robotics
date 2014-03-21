0c04f2706ebdd2d961b5c968becb61e4a5c60bfa
t.nandan2008@gmail.com <t.nandan2008@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-03-21 16:13:09 +0000

    POC on Data Return completed sucessfully. Check SaveFragment.java for details

--- 2013_14/FRCGameScoutingApp/FRCGameScoutFinal/app/src/main/java/com/lmrobotics/frcgamescoutfinal/app/MainActivity.java
+++ 2013_14/FRCGameScoutingApp/FRCGameScoutFinal/app/src/main/java/com/lmrobotics/frcgamescoutfinal/app/MainActivity.java
@@ -102,6 +102,8 @@ public class MainActivity extends Activity {
                     return SetupFragment.newInstance();
                 case 1:
                     return AutoFragment.newInstance();
+                case 4:
+                    return SaveFragment.newInstance();
                 default:
                     return PlaceholderFragment.newInstance(position+1);
             }
