55c9f7a098b05ea41476cb84d884e3c2efd0e43b
lmroboticssw@gmail.com <lmroboticssw@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-10-04 14:48:21 +0000

    got nav6 working

---
+++ 2013_14/robotCode/IterativeTemplate/IMUProcess.h
@@ -0,0 +1,39 @@
+#ifndef IMUPROCESS_H_
+#define IMUPROCESS_H_
+
+#define ROLL_HISTORY_LENGTH 10
+#define PITCH_HISTORY_LENGTH 10
+
+#include "IMUAdvanced.h"
+#include "Synchronized.h"
+
+class IMUProcess : public IMUAdvanced{
+	//friend class IMU;
+	//friend class IMUAdvanced;
+public:
+	IMUProcess(SerialPort *pport, uint8_t update_rate_hz);
+	~IMUProcess(void);
+	
+	virtual float GetPitch();       // Pitch, in units of degrees (-180 to 180)
+	virtual float GetRoll();        // Roll, in units of degrees (-180 to 180)
+	
+	void ResetYawRollPitch(void);
+	virtual int DecodePacketHandler( char *received_data, int bytes_remaining );
+protected:
+	void UpdateRollHistory(float curr_Roll);
+	void UpdatePitchHistory(float curr_Pitch);
+	
+	double roll_offset;
+	double pitch_offset;
+	
+	int next_roll_history_index;
+	int next_pitch_history_index;
+	
+	float roll_history[ROLL_HISTORY_LENGTH];
+	float pitch_history[PITCH_HISTORY_LENGTH];
+private:
+	double GetAverageFromPitchHistory();
+	double GetAverageFromRollHistory();
+};
+
+#endif
