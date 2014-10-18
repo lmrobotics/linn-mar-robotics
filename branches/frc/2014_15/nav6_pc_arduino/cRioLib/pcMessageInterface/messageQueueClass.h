665de07b83c6efa21986c4cab09182be660a49c4
raflood61 <raflood61@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-10-18 16:28:13 +0000

    Message Passing Library for cRio.  cRio needs to declare udpServerMessageTransport and be the message server

---
+++ branches/frc/2014_15/nav6_pc_arduino/cRioLib/pcMessageInterface/messageQueueClass.h
@@ -0,0 +1,35 @@
+#ifndef MESSAGE_QUEUE_CLASS_H
+#define MESSAGE_QUEUE_CLASS_H
+
+#include "BaseMessageClass.h"
+#include <queue>
+
+using namespace Messages;
+
+class messageQueueClass{
+
+public:
+    messageQueueClass();
+    ~messageQueueClass();
+
+    /** ***************************************************************************
+    * \brief Provide an indication if whether a message is available or not.
+    */
+    bool messageAvailable();
+
+    /** ***************************************************************************
+    * \brief Return a message from message queue.
+    */
+    BaseMessageClass* getMessage();
+
+    /** ***************************************************************************
+    * \brief Add a message to message queue.
+    */
+    void putMessage(BaseMessageClass *msg);
+
+private:
+    std::queue<BaseMessageClass*> msgQueue;
+    bool available;
+};
+
+#endif
