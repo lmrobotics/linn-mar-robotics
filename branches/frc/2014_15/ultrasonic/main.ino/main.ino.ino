5348a1c0d225a9f3b12c10e9d277b9c6b46c12ff
lmroboticssw@gmail.com <lmroboticssw@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-11-18 18:29:38 +0000

    ultrasonic_sensor_main_finished

--- branches/frc/2014_15/ultrasonic/main.ino/main.ino.ino
+++ branches/frc/2014_15/ultrasonic/main.ino/main.ino.ino
@@ -3,14 +3,20 @@
 
 message* gMsg;
 Led* gLed;
+int gDistance;
 
 void setup() {
   pinMode(CRIO_PORT1, INPUT);
   pinMode(CRIO_PORT2, INPUT);
   pinMode(CRIO_PORT3, INPUT);
+  pinMode(LED_PORT1, OUTPUT);
+  pinMode(LED_PORT2, OUTPUT);
   gMsg = new message();
   gLed = new Led();
 }
 void loop() {
- 
+ if(gMsg->ifAvalable()){
+   gDistance = gMsg->getMessage();
+   gLed->WriteDistanceToLed(gDistance);
+ }
 }
