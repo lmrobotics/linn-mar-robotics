fb80bdefe69940653283091b067d66a86c82c83b
lmroboticssw@gmail.com <lmroboticssw@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-11-19 00:24:33 +0000

    dinamic_chagnes

--- branches/frc/2014_15/dinamicMessages/dimamicMessages/MessageLoader.cpp
+++ branches/frc/2014_15/dinamicMessages/dimamicMessages/MessageLoader.cpp
@@ -1,1 +1,45 @@
 #include "MessageLoader.h"
+
+using namespace Message;
+
+MessageLoader::MessageLoader(){
+
+}
+
+void MessageLoader::Run(void){
+
+}
+
+void* MessageLoader::DecodeFloat(int8* rawData){
+    return 0;
+}
+
+int8* MessageLoader::EncodeFloat(void* Float){
+    return 0;
+}
+
+void* MessageLoader::DecodeInt32(int8* rawData){
+    int32 i32 = ((int32)message[3]<<24) +
+            ((int32)message[2]<<16) +
+            ((int32)message[1]<<8) +
+             (int32)message[0];
+
+    return (void*)&i32;
+}
+
+int8* MessageLoader::EncodeInt32(void* Int32){
+    return 0;
+}
+
+void* MessageLoader::DecodeInt16(int8* rawData){
+    int16 i16 = (rawData[1]<<8) + rawData[0];
+    return (void*)&i16;
+}
+
+int8* MessageLoader::EncodeInt16(void* Int16){
+    return 0;
+}
+
+MessageLoader::~MessageLoader(){
+
+}
--- branches/frc/2014_15/dinamicMessages/dimamicMessages/MessageLoader.h
+++ branches/frc/2014_15/dinamicMessages/dimamicMessages/MessageLoader.h
@@ -1,4 +1,32 @@
 #ifndef MESSAGE_LOADER_H
 #define MESSAGE_LOADER_H
 
+#define CALL_DECODE_FUNCTION(messageLoader, function) ((messageLoader)->*(function))
+#define CALL_ENCODE_FUNCTION(messageLoader, function) ((messageLoader)->*(function))
+
+namespace Message{
+
+   typedef char int8;
+   typedef int int16;
+   typedef long int32;
+   typedef unsigned char uint8;
+   typedef unsigned int uint16;
+   typedef unsigned long uint32;
+
+    class MessageLoader{
+     public:
+        typedef void& (MessageLoader::*DecodeFunction)(int8* rawData);
+        typedef int8* (MessageLoader::*EncodeFunction)(void* inData);
+        MessageLoader();
+        ~MessageLoader(void);
+        void Run(void);
+     private:
+        void& DecodeFloat(int8* rawData);
+        int8* EncodeFloat(void* Float);
+        void& DecodeInt32(int8* rawData);
+        int8* EncodeInt32(void* Int32);
+        void& DecodeInt16(int8* rawData);
+        int8* EncodeInt16(void* Int16);
+    };
+}
 #endif
