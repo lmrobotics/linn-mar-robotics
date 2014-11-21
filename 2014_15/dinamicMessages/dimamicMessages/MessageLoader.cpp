93ebb4c0d11565c9b80d7b8297a3f5806a45147a
lmroboticssw@gmail.com <lmroboticssw@gmail.com@8aa245d1-972c-41d0-c14e-e24111e5e9b2>
2014-11-21 00:01:05 +0000

    Message_loader_changes

--- 2014_15/dinamicMessages/dimamicMessages/MessageLoader.cpp
+++ 2014_15/dinamicMessages/dimamicMessages/MessageLoader.cpp
@@ -1,6 +1,6 @@
 #include "MessageLoader.h"
 
-using namespace Message;
+using namespace message;
 
 MessageLoader::MessageLoader(){
 
@@ -10,34 +10,81 @@ void MessageLoader::Run(void){
 
 }
 
-void* MessageLoader::DecodeFloat(int8* rawData){
-    return 0;
+MessageHeader MessageLoader::DecodeMsgHeader(int8* rawData, int offset){
+    int16 size = DecodeInt16(rawData, 0);
+    string heading = DecodeString(rawData, size, offset + sizeof(int16));
+    MessageHeader msgHead;
+    msgHead.messageHeading = heading;
+    msgHead.size = size;
+    unsigned lessId = 'd';
+    unsigned greaterId = 'd';
+    char buffer[6];
+    for(int i = 0; i < MESSAGE_HEADING_LENGTH; i++){
+        lessId = heading.find("<");
+        greaterId = heading.find(">");
+        heading.copy(buffer, greaterId - 1, lessId + 1);
+    }
+
+    return msgHead;
 }
 
-int8* MessageLoader::EncodeFloat(void* Float){
-    return 0;
+void MessageLoader::EncodeMsgHeader(int8* rawData, MessageHeader msgHead, int offset){
+
 }
 
-void* MessageLoader::DecodeInt32(int8* rawData){
-    int32 i32 = ((int32)message[3]<<24) +
-            ((int32)message[2]<<16) +
-            ((int32)message[1]<<8) +
-             (int32)message[0];
-
-    return (void*)&i32;
+string MessageLoader::DecodeString(int8* rawData, int size, int offset){
+    int8* rawString = new int8[size];
+    for(int i = 0; i < size; i++){
+        rawString[i] = rawData[offset + i];
+    }
+    string val(rawString);
+    return val;
 }
 
-int8* MessageLoader::EncodeInt32(void* Int32){
-    return 0;
+void MessageLoader::EncodeString(string String,int8* rawData, int offset){
+    const int8* rawString = String.c_str();
+    for(unsigned int i = 0; i < String.size(); i++){
+        rawData[offset + i] = rawString[i];
+    }
 }
 
-void* MessageLoader::DecodeInt16(int8* rawData){
-    int16 i16 = (rawData[1]<<8) + rawData[0];
-    return (void*)&i16;
+float MessageLoader::DecodeFloat(int8* rawData, int offset){
+    int16 exp = DecodeInt16(rawData, offset);
+    int32 mantissa = DecodeInt32(rawData, offset + 2);
+    return ldexp(((float)mantissa)/(float)2147483647.0, exp);
 }
 
-int8* MessageLoader::EncodeInt16(void* Int16){
-    return 0;
+void MessageLoader::EncodeFloat(float Float, int8* rawData, int offset){
+    int exp;
+    float mantissa = frexp(Float, &exp);
+    EncodeInt16(exp, rawData, offset);
+    EncodeInt32(mantissa*2147483647.0, rawData, offset+2);
+}
+
+int32 MessageLoader::DecodeInt32(int8* rawData, int offset){
+    int32 i32 = ((int32)rawData[offset + 3]<<24) +
+            ((int32)rawData[offset + 2]<<16) +
+            ((int32)rawData[offset + 1]<<8) +
+             (int32)rawData[offset];
+
+    return i32;
+}
+
+void MessageLoader::EncodeInt32(int32 Int32, int8* rawData, int offset){
+    rawData[offset] = Int32 & 0xFF;
+    rawData[offset + 1] = (Int32>>8) & 0xFF;
+    rawData[offset + 2] = (Int32>>16) & 0xFF;
+    rawData[offset + 3] = (Int32>>24) & 0xFF;
+}
+
+int16 MessageLoader::DecodeInt16(int8* rawData, int offset){
+    int16 i16 = (rawData[offset + 1]<<8) + rawData[offset];
+    return i16;
+}
+
+void MessageLoader::EncodeInt16(int16 Int16, int8* rawData, int offset){
+    rawData[offset] = Int16 & 0xFF;
+    rawData[offset + 1] = (Int16>>8) & 0xFF;
 }
 
 MessageLoader::~MessageLoader(){
--- 2014_15/dinamicMessages/dimamicMessages/main.cpp
+++ 2014_15/dinamicMessages/dimamicMessages/main.cpp
@@ -4,9 +4,20 @@
 #include "MessageInterfaceServer.h"
 #include "MessageLoader.h"
 
+using namespace message;
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    string val("<m>");
+    size_t e = val.rfind("<");
+    size_t t = val.rfind(">");
+    char buffer[23];
+    std::size_t length = val.copy(buffer, t - 1, e + 1);
+    buffer[length]='\0';
+    string val2(buffer);
+
+    printf(val2.c_str());
     return a.exec();
 }
